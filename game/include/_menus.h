d9f8e3a4be1708c1cbde49cba391c9276522b673
AshtonVang001 <radturkdude@gmail.com>
2025-11-18 11:43:28 -0800

    add: init menu functions

--- game/include/_menus.h
+++ game/include/_menus.h
@@ -1,12 +1,34 @@
 #ifndef _MENUS_H
 #define _MENUS_H
 
+#include <_textureLoader.h>
+
 
 class _menus
 {
     public:
         _menus();
         virtual ~_menus();
+
+        _textureLoader *myTex = new _textureLoader();
+
+
+        void initImage(char*);
+        void menuDraw(float,float);
+        void helpDraw(float, float);
+        void popUpDraw(float, float, char*);
+
+
+        void HUD(char*, double);
+
+        float  screenWidth, screenHeight;
+
+        float texCrop = 1.0;
+        float verCrop = 1.0;
+        float xMax = 1.0;
+        float xMin = 0.0;
+        float yMax = 1.0;
+        float yMin = 0.0;
 
     protected:
 
