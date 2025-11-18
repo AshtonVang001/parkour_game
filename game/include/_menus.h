16e3af6e684bb68bb9aedb2e5491128148ec9c54
AshtonVang001 <radturkdude@gmail.com>
2025-11-18 11:19:38 -0800

    add: add menus class

---
+++ game/include/_menus.h
@@ -0,0 +1,16 @@
+#ifndef _MENUS_H
+#define _MENUS_H
+
+
+class _menus
+{
+    public:
+        _menus();
+        virtual ~_menus();
+
+    protected:
+
+    private:
+};
+
+#endif // _MENUS_H
---
+++ game/src/_menus.cpp
@@ -0,0 +1,11 @@
+#include "_menus.h"
+
+_menus::_menus()
+{
+    //ctor
+}
+
+_menus::~_menus()
+{
+    //dtor
+}
